d764920c4f996c578c5412363e75f812c556939b
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-03-13 23:59:31 +0800

    format

--- uploadpage/components/shown.tsx
+++ uploadpage/components/shown.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function() {
-	return (
-		<img/>
-	)
+export default function () {
+  return <img />;
 }
--- uploadpage/pages/index.tsx
+++ uploadpage/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Uploader from '~/components/upload.tsx'
+import Uploader from "~/components/upload.tsx";
 export default function Home() {
   return (
     <div className="page">
       <head>
         <title>Hello World - Aleph.js</title>
       </head>
-			<Uploader />
+      <Uploader />
     </div>
   );
 }
